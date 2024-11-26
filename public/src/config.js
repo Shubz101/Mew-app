e4187906275a99e44819414d2cc5bb05a35d210f
Shubz101 <shubzzz1019@gmail.com>
2024-11-26 17:20:44 +0530

    Create config.js

---
+++ public/src/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  ALPHA_VANTAGE_API_KEY: process.env.REACT_APP_ALPHA_VANTAGE_API_KEY,
+  BASE_URL: 'https://www.alphavantage.co/query',
+  DEFAULT_CURRENCY: 'INR',
+  STOCK_REFRESH_INTERVAL: 5 * 60 * 1000, // 5 minutes
+};
